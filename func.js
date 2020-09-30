ca0ba20af7895c2628f085942791237996166797
Pásztor Krisztián <pasztor.krisztian@szbisz.local>
2020-09-30 15:00:19 +0200

    Kiegészítés

--- func.js
+++ func.js
@@ -11,20 +11,53 @@ const diakok=[
     {"nev": "Németh Christopher", "osztaly": "14SZF"},
 ];
 
-let table="<table><tr><th>név</th><th>osztály</th></tr>";
-for(let i=0; i<diakok.length; i++){
-table+="<tr>";
-table+="<td>"+diakok[i].nev+"</td>";
-table+="<td>"+diakok[i].osztaly+"</td>";
-table+="</tr>";
+function osztalytoltes()
+{
+    let table="<table><tr><th>név</th><th>osztály</th></tr>";
+    for(let i=0; i<diakok.length; i++){
+    table+="<tr>";
+    table+="<td>"+diakok[i].nev+"</td>";
+    table+="<td>"+diakok[i].osztaly+"</td>";
+    table+="</tr>";
 }
-table+="</table>";
-document.getElementById("táblázat").innerHTML=table;
+    table+="</table>";
+    document.getElementById("táblázat").innerHTML=table;
+}
 function feltolt(){
     let nev=document.getElementById('nev').value;
     let osztaly=document.getElementById('osztaly').value;
-    console.log(nev+' '+osztaly);
-    diakok.push({'osztaly': `${osztaly}`, 'nev': `${nev}`})
-
+    //console.log(nev+' '+osztaly);
+    let nevTomb=[];
+    diakok.forEach((diakok)=>{
+        nevTomb.push(diakok.nev);
+    });
+    if(nevTomb.includes(nev)){
+        window.alert('Ez a név szerepel már a listában!');
+    }
+    else{
+        diakok.push({'osztaly': `${osztaly}`, 'nev': `${nev}`});
+    }
+    /*let eldont=false;
+    diakok.forEach((diakok)=>{
+        if(diakok.nev==nev){
+            eldont=true;
+            return;
+        }
+        /*window.alert('Ez a név szerepel már a listában!');
+        return;
+    });
+    if(eldont){
+        window.alert('Ez a név szerepel már a listában!');
+    }
+    else{
+        diakok.push({'osztaly': `${osztaly}`, 'nev': `${nev}`});
+    }*/
+    osztalytoltes();
 }
 feltolt();
+
+function torol()
+{
+    diakok.pop();
+    osztalytoltes();
+}
